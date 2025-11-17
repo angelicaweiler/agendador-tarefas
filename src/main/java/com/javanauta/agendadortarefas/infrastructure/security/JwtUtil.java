97cd263af1ff2198ea2b294a18817787beda4ee3
Angelica <angelicaweilerw@gmail.com>
2025-11-17 17:54:32 -0300

    alterado dockerfile

--- src/main/java/com/javanauta/agendadortarefas/infrastructure/security/JwtUtil.java
+++ src/main/java/com/javanauta/agendadortarefas/infrastructure/security/JwtUtil.java
@@ -3,11 +3,9 @@ package com.javanauta.agendadortarefas.infrastructure.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Date;
 
