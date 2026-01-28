6bbb5198061e8ec77af01d454f86d98b4572d732
Angelica <angelicaweilerw@gmail.com>
2026-01-28 11:03:40 -0300

    inclusão de consumer do kafka

---
+++ src/main/java/com/javanauta/agendadortarefas/infrastructure/message/config/KafkaConfig.java
@@ -0,0 +1,4 @@
+package com.javanauta.agendadortarefas.infrastructure.message.config;
+
+public class KafkaConfig {
+}
---
+++ src/main/java/com/javanauta/agendadortarefas/infrastructure/message/consumer/KafkaConsumer.java
@@ -0,0 +1,4 @@
+package com.javanauta.agendadortarefas.infrastructure.message.consumer;
+
+public class KafkaConsumer {
+}
