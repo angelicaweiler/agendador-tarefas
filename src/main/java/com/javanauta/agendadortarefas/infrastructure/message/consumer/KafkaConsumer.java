46f004294d38749da814004f5c8b6059c0662acc
Angelica <angelicaweilerw@gmail.com>
2026-03-03 15:23:22 -0300

    add codigo Mensageria

--- src/main/java/com/javanauta/agendadortarefas/infrastructure/message/consumer/KafkaConsumer.java
+++ src/main/java/com/javanauta/agendadortarefas/infrastructure/message/consumer/KafkaConsumer.java
@@ -1,4 +1,21 @@
 package com.javanauta.agendadortarefas.infrastructure.message.consumer;
 
+import com.javanauta.agendadortarefas.business.TarefasService;
+import com.javanauta.agendadortarefas.business.dto.TarefasDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class KafkaConsumer {
+
+    private final TarefasService tarefasService;
+
+    @KafkaListener(topics = "topico.status",
+    groupId = "agendador-tarefas",
+    containerFactory = "kafkaListenerContainerFactory")
+    public void receberMensagem(TarefasDTO tarefasDTO){
+        tarefasService.alteraStatus(tarefasDTO.getStatusNotificacaoEnum(), tarefasDTO.getId());
+    }
 }
